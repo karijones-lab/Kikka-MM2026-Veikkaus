e4df6a991a48ef41aecb62055180de4db3550be0
karijones-lab <kari.jones@2outsiders.com>
2026-06-02 01:24:49 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,14 @@ import { initializeApp } from "firebase/app";
 import { initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "SINUN",
-  authDomain: "SINUN",
-  projectId: "SINUN",
-  storageBucket: "SINUN",
-  messagingSenderId: "SINUN",
-  appId: "SINUN"
+  apiKey: "AIzaSyDHCmGAL9Wl6VKhDynbpmicUS0bYc6ivwg",
+  authDomain: "kikka-mm2026-kisaveikkaus.firebaseapp.com",
+  databaseURL: "https://kikka-mm2026-kisaveikkaus-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "kikka-mm2026-kisaveikkaus",
+  storageBucket: "kikka-mm2026-kisaveikkaus.firebasestorage.app",
+  messagingSenderId: "568046283394",
+  appId: "1:568046283394:web:3a1ce9fc8d63253727629b",
+  measurementId: "G-GPYDDEM424"
 };
 
 console.log("🔥 PROJECT ID:", firebaseConfig.projectId);
