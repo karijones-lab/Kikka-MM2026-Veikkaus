876df4213d594143a49e7b59c5628e61a9b43ecd
karijones-lab <kari.jones@2outsiders.com>
2026-06-02 00:53:05 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -13,6 +13,23 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // 🔥 tämä ratkaisee timeoutin
+import { initializeApp } from "firebase/app";
+import { initializeFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "SINUN",
+  authDomain: "SINUN",
+  projectId: "SINUN",
+  storageBucket: "SINUN",
+  messagingSenderId: "SINUN",
+  appId: "SINUN"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// 🔥 LISÄÄ HOST
 export const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true
+  experimentalForceLongPolling: true,
+  host: "firestore.googleapis.com",
+  ssl: true
 });
