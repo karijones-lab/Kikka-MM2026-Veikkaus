58d62d3fde3975059be8ceeef09aab1aaba982f7
karijones-lab <kari.jones@2outsiders.com>
2026-07-16 12:22:30 +0300

    Update Bracket.jsx

--- src/Bracket.jsx
+++ src/Bracket.jsx
@@ -278,7 +278,7 @@ const manualFinal = [
   winner: ""
 };
 
-const DEADLINE = new Date("2026-07-14T21:30:00");
+const DEADLINE = new Date("2026-07-16T12:30:00");
 const isLocked = new Date() > DEADLINE;
 
     return (
